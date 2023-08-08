13166ea3acbd63c3591db4702976494f5e300401
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-08-08 17:21:56 +0200

    routing working. creating branches

--- HTL_Weiz_Website/src/components/NavBar.vue
+++ HTL_Weiz_Website/src/components/NavBar.vue
@@ -2,66 +2,140 @@
   <div>
     <div class="uk-position-relative">
       <div class="uk-position-top">
-        <div uk-sticky="start: 200; animation: uk-animation-slide-top; sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; cls-inactive: uk-navbar-transparent uk-light">
+        <div
+          uk-sticky="start: 200; animation: uk-animation-slide-top; sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; cls-inactive: uk-navbar-transparent uk-light"
+        >
           <nav class="uk-navbar-container" uk-navbar>
-          <div class="uk-container">
-            <div uk-navbar>
-              <div class=".uk-navbar-center">
-                <ul class="uk-navbar-nav">
-                  <li class="uk-logo">
-                    <img class="img-logo" src="/src/assets/images/Logo_HTL_2013_Large.png" alt="Logo">
-                  </li>
-                  <li>
-                    <a href="#">Aktuelles</a>
-                    <div class="uk-navbar-dropdown">
-                      <ul class="uk-nav uk-navbar-dropdown-nav">
-                        <li><a href="#">Schulveranstaltungen</a></li>
-                        <li><a href="#">News</a></li>
-                        <li><a href="#">Erasmus+</a></li>
-                      </ul>
+            <div class="uk-container">
+              <div uk-navbar>
+                <div class=".uk-navbar-center">
+                  <ul class="uk-navbar-nav">
+                    <li>
+                      <a class="uk-logo"
+                        ><img
+                          src="src\assets\images\Logo_HTL_2013_Small.png"
+                          width="100"
+                          height=""
+                          alt="" /><router-link to="/"></router-link
+                      ></a>
+                    </li>
+                    <li>
+                      <a href="#">Aktuelles</a>
+                      <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                          <li>
+                            <router-link to="/schulveranstaltungen"
+                              >Schulveranstaltungen</router-link
+                            >
+                          </li>
+                          <li><router-link to="/news">News</router-link></li>
+                          <li>
+                            <router-link to="/erasmus">Erasmus+</router-link>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li>
+                      <a href="#">Ausbildung</a>
+                      <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                          <li>
+                            <router-link to="/wi"
+                              >Wirtschaftsingenieure</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/et">Elektrotechnik</router-link>
+                          </li>
+                          <li>
+                            <router-link to="/it"
+                              >Informationstechnologie</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/ut"
+                              >Maschinenbau - Umwelttechnik</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/mb"
+                              >Maschinenbau - Anlagentechnik</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/at"
+                              >Maschinenbau -
+                              Automatisierungstechnik</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/awi"
+                              >Abendschule - Wirtschaftsingenieure</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/mt"
+                              >Fachschule - Mechatronik</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/itk"
+                              >Kolleg - Informatik</router-link
+                            >
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li><router-link to="/team">Team</router-link></li>
+                    <li>
+                      <router-link to="/ansprechpartner"
+                        >AnsprechpartnerInnen</router-link
+                      >
+                    </li>
+                    <li>
+                      <a href="#">Services</a>
+                      <div class="uk-navbar-dropdown">
+                        <ul class="uk-nav uk-navbar-dropdown-nav">
+                          <li>
+                            <router-link to="/students"
+                              >SchülerInnen</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/teachers"
+                              >LehrerInnen</router-link
+                            >
+                          </li>
+                          <li>
+                            <router-link to="/parents">Eltern</router-link>
+                          </li>
+                          <li>
+                            <router-link to="/anmeldung">Anmeldung</router-link>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                    <div class="uk-margin">
+                      <form class="uk-search uk-search-default">
+                        <a
+                          href=""
+                          class="uk-search-icon-flip"
+                          uk-search-icon
+                        ></a>
+                        <input
+                          class="uk-search-input rounded uk-margin-left"
+                          type="search"
+                          placeholder="Search"
+                          aria-label="Search"
+                        />
+                      </form>
                     </div>
-                  </li>
-                  <li>
-                    <a href="#">Ausbildung</a>
-                    <div class="uk-navbar-dropdown">
-                      <ul class="uk-nav uk-navbar-dropdown-nav">
-                        <li><a href="#">Wirtschaftsingenieure</a></li>
-                        <li><a href="#">Elektrotechnik</a></li>
-                        <li><a href="#">Informationstechnologie</a></li>
-                        <li><a href="#">Maschinenbau - Umwelttechnik</a></li>
-                        <li><a href="#">Maschinenbau - Anlagentechnik</a></li>
-                        <li><a href="#">Maschinenbau - Automatisierungstechnik</a></li>
-                        <li><a href="#">Abendschule - Wirtschaftsingenieure</a></li>
-                        <li><a href="#">Kolleg - Informatik</a></li>
-                        <li><a href="#">Fachschule - Mechatronik</a></li>
-                      </ul>
-                    </div>
-                  </li>
-                  <li><a href="#">Schule</a></li>
-                  <li><a href="#">Ansprechpartner</a></li>
-                  <li>
-                    <a href="#">Services</a>
-                    <div class="uk-navbar-dropdown">
-                      <ul class="uk-nav uk-navbar-dropdown-nav">
-                        <li><a href="#">Schüler:innen</a></li>
-                        <li><a href="#">Lehrer:innen</a></li>
-                        <li><a href="#">Eltern</a></li>
-                        <li><a href="#">Anmeldung</a></li>
-                      </ul>
-                    </div>
-                  </li>
-                  <div class="uk-margin">
-                    <form class="uk-search uk-search-default">
-                      <a href="" class="uk-search-icon-flip" uk-search-icon></a>
-                      <input class="uk-search-input rounded uk-margin-left" type="search" placeholder="Search" aria-label="Search">
-                    </form>
-                  </div>
-                </ul>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-        </nav>
-      </div>
+          </nav>
+        </div>
       </div>
     </div>
   </div>
@@ -71,7 +145,7 @@
 /* import ... from ...; */
 
 export default {
-  name: 'Navbar',
+  name: "Navbar",
   components: {
     /*,*/
   },
