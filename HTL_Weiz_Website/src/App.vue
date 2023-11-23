a75827a7e0a104c1809c7b46ba8696e8e7c6edaa
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-11-23 07:43:45 +0100

    colors

--- HTL_Weiz_Website/src/App.vue
+++ HTL_Weiz_Website/src/App.vue
@@ -15,38 +15,4 @@ export default defineComponent({
 </script>
 
 <style>
-
-.padtop {
-  padding-top: 25px;
-}
-.padbot {
-  padding-bottom: 25px;
-}
-.padleftright {
-  padding-left: 100px;
-  padding-right: 100px;
-}
-.roundedl {
-  border-radius: 18px;
-}
-
-@media (max-width: 640px) {
-.padtop {
-  padding-top: 10px;
-}
-.padbot {
-  padding-bottom: 10px;
-}
-.padleftright {
-  padding-left: 35px;
-  padding-right: 35px;
-}
-}
-
-@media (min-width: 1500px) {
-.padleftright {
-  padding-left: calc(20% - 150px);
-  padding-right: calc(20% - 150px);
-}
-}
 </style>
